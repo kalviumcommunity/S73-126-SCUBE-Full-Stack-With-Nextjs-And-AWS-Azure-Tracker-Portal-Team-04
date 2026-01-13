ebf41109d52eb692729742c3d62ff4c60a85fc32
Watcheer <shubhampadkonde12@gmail.com>
2026-01-13 16:01:36 +0530

    2.27 Completed

---
+++ internship-portal/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <Button label="Click Me" />
+    </div>
+  );
+}
